eb496cfc9a21c61a7e1c9f59138b91982ac451b2
Daichi Sasaki <104604479+dvichisasaki@users.noreply.github.com>
2023-01-10 19:51:19 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+{
+function play(){
+
+    
+
+    setTimeout(() => {
+            images[currentIndex].classList.remove('current');
+            currentIndex++;
+            if (currentIndex > images.length - 1){
+                currentIndex = 0;
+            }
+            images[currentIndex].classList.add('current');
+            play();
+        }, 1000);
+}
+const images = document.querySelectorAll('.hero img');
+    let currentIndex = 0;
+
+    play();
+}
+
+const open = document.getElementById('open');
+    const overlay = document.querySelector('.overlay');
+    const close = document.getElementById('close');
+
+    open.addEventListener('click', () => {
+        overlay.classList.add('show');
+        open.classList.add('hide');
+    });
+
+    close.addEventListener('click', () => {
+        overlay.classList.remove('show');
+        open.classList.remove('hide');
+    })
